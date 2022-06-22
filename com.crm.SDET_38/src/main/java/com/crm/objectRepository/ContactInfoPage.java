c90e78c311b77659f36c4bf6f15c49163a47b6fb
Aqeeb <Aqeeb@LAPTOP-I43I8TK8>
2022-06-22 15:56:39 +0530

    commit conflict

---
+++ com.crm.SDET_38/src/main/java/com/crm/objectRepository/ContactInfoPage.java
@@ -0,0 +1,30 @@
+package com.crm.objectRepository;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ContactInfoPage {
+
+	//Declaration
+	//updated
+	@FindBy(xpath="//span[contains(text(),'Contact Information')]")
+	private WebElement contactHeaderTxt;
+	
+	//Initialization
+	public ContactInfoPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+	
+	//Utilization
+	public WebElement getContactHeaderTxt() {
+		return contactHeaderTxt;
+	}
+	
+	public String checkContactIsCreatedOrNot() {
+		String AText = contactHeaderTxt.getText();
+		return AText;
+	}	
+	
+}
